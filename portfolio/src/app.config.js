b780c3e0a0b410a1cb77ccf11cac5debaa29a575
Gabriel <pedromurillo96@gmail.com>
2019-10-15 00:14:26 -0700

    changes made for server

--- portfolio/src/app.config.js
+++
@@ -1,6 +0,0 @@
-// export default {
-//     issuer: "https://dev-264100.okta.com/oauth2/default",
-//     clientId: "0oa19ydanvXbR9eeL357",
-//     redirectUri: window.location.origin + "/implicit/callback",
-//     url: "http://localhost:3000"
-// }
