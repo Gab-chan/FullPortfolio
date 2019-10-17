02d00846a7fa52334efb5faae27be8612cad7b55
Gabriel <pedromurillo96@gmail.com>
2019-10-17 15:28:33 -0700

    portfolio page updates

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -4,12 +4,6 @@ import ItemJumbotron from "../components/ItemJumbotron";
 import { Form, FormGroup, Label, Input, } from 'reactstrap';
 import ModalExample from "../components/Modal/index"
 
-function searchingFor(term) {
-    return function(x){
-      return x.name.toLowerCase().includes(term.toLowerCase()) || x.postedBy.toLowerCase().includes(term.toLowerCase()) || false;
-    }
-  }
-
 class Portfolio extends React.Component {
   constructor(props){
     super(props);
@@ -20,13 +14,9 @@ class Portfolio extends React.Component {
     itemClicked: "",
     modal: false,
   }
-  this.searchHandler = this.searchHandler.bind(this);
+ 
 };
 
-  searchHandler(event){
-      this.setState({ term : event.target.value })
-      console.log(event.target.value);
-  };
 
 handleClicked = id => {
     // console.log("You click id " + id);
@@ -61,9 +51,13 @@ handleClicked = id => {
         <ModalExample items={this.state.items} item={this.state.itemClicked} sendEmailToHome={this.handleDataFromModal} handleClicked={this.handleClicked} newModal={this.state.modal} newToggle={this.toggle}></ModalExample>
         
         <ItemJumbotron className="jumbo-background">
-        <Label className="text1" for="exampleSearch">About Me</Label>
-        <img className="logo" src="/box3.png" alt="logo"/>
-        <p className="statement">Front-End Web Developer with customer service background that makes for great communications skills and outstanding teamwork abilities. Recently earned a certificate in Full Stack Development from the ,University of Arizona, with newly developed skills in JavaScript, CSS, React.js, and responsive web design. Capable of critical thinking, original problem solving skills and multi-task. My objective on each project is to produce an experience that can be fun for anyone interacting with my work. On my last project, I worked on the design of a website made with React.js and using mainly CSS for the visuals. Excited to learn new languages and tools that will help in the development of a long and fun carrier. Fluent and capable of writing in both english and spanish.</p>
+        <img className="logo" src="/projectimage.jpg" alt="project1"/>
+        <img className="logo" src="/maxresdefault.jpg" alt="trivia"/>
+        <img className="logo" src="/free-easy-gifs.gif" alt="gify"/>
+        <img className="logo" src="/banner_big_tpg.jpg" alt="banner"/>
+        <img className="logo" src="/project2.jpg" alt="project2"/>
+        <img className="logo" src="/projectimage.jpg" alt="project2"/>
+        
         </ItemJumbotron>
       </div>
     );
