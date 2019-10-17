4c2faf81616953b5e5bae87999b9cfe733d362c4
Gabriel <pedromurillo96@gmail.com>
2019-10-17 15:13:23 -0700

    portfolio page created and home page update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/home.js";
-// import Portfolio from "./pages/porfolio.js";
+import Portfolio from "./pages/portfolio.js";
 import Nav from "./components/Nav";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Nav />
         <Route exact path="/" component={Home} />
-        {/* <Route exact path="/portfolio" component={Portfolio} /> */}
+        <Route exact path="/portfolio" component={Portfolio} />
 
     </Router>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -2,63 +2,16 @@ import React from "react";
 import "./home.css";
 import ItemJumbotron from "../components/ItemJumbotron";
 import { Form, FormGroup, Label, Input, } from 'reactstrap';
-import ModalExample from "../components/Modal/index"
-
-function searchingFor(term) {
-    return function(x){
-      return x.name.toLowerCase().includes(term.toLowerCase()) || x.postedBy.toLowerCase().includes(term.toLowerCase()) || false;
-    }
-  }
 
 class Home extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-    items: [],
-    term: "",
-    idClicked: "",
-    itemClicked: "",
-    modal: false,
-  }
-  this.searchHandler = this.searchHandler.bind(this);
-};
-
-  searchHandler(event){
-      this.setState({ term : event.target.value })
-      console.log(event.target.value);
-  };
-
-handleClicked = id => {
-    // console.log("You click id " + id);
-    this.setState({idClicked: id});
-    for (let i = 0; i < this.state.items.length; i++){
-      if(this.state.items[i]._id === id){
-        this.setState({itemClicked: this.state.items[i]});
-        console.log(this.state.itemClicked);
-      }
-    }
-    this.setState({
-      modal: !this.state.modal
-    });
-    // console.log(this.state.modal)
-};
-
-  toggle = () => {
-    this.setState({
-      modal: !this.state.modal
-    });
-  }
 
   render() {
-    const {term, items} = this.state;
     return (
       <div className="main">
 
         <div className="heading">
           <h3 className="heading-text">Pedro G Murillo</h3>
         </div>
-      
-        <ModalExample items={this.state.items} item={this.state.itemClicked} sendEmailToHome={this.handleDataFromModal} handleClicked={this.handleClicked} newModal={this.state.modal} newToggle={this.toggle}></ModalExample>
         
         <ItemJumbotron className="jumbo-background">
         <Label className="text1" for="exampleSearch">About Me</Label>
